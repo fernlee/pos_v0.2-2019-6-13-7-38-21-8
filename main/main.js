f1c0ba873032432ec8a2d99c1dcfc9e314fbff73
yaeli <yaeli@thoughtworks.com>
2019-06-19 23:00:52 +0800

    [yae][implement the function]

--- main/main.js
+++ main/main.js
@@ -1,5 +1,22 @@
 'use strict';
 
 function printReceipt(inputs) {
-  console.log('请在此实现练习要求，并改写该行代码。');
+  const countedItems = inputs.reduce((allItems, currentIem) => {
+        allItems[currentIem] ? allItems[currentIem]++ : allItems[currentIem] = 1;
+        return allItems;
+    }, {});
+  let totalPrice = 0;
+  const output = Object.keys(countedItems).map(input => {
+      const currentItem = loadAllItems().find(item => item.barcode === input);
+      const {name, unit, price} = currentItem;
+      const quantity = countedItems[input];
+      const currentPrice = price*quantity;
+      totalPrice = totalPrice + currentPrice;
+      return `名称：${name}，数量：${quantity}${unit}，单价：${price.toFixed(2)}(元)，小计：${currentPrice.toFixed(2)}(元)`
+  }).join("\n");
+    console.log(`***<没钱赚商店>收据***
+${output}
+----------------------
+总计：${totalPrice.toFixed(2)}(元)
+**********************`);
 }
