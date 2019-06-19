a54f9184b73f92c996b94dafbf054c45647b8f91
yaeli <yaeli@thoughtworks.com>
2019-06-20 00:02:40 +0800

    [yae][refactor the code to generate the receipt array and map it the output]

--- main/main.js
+++ main/main.js
@@ -1,19 +1,19 @@
 'use strict';
 
 function printReceipt(inputs) {
-  const countedItems = inputs.reduce((allItems, currentIem) => {
+    const countedItems = inputs.reduce((allItems, currentIem) => {
         allItems[currentIem] ? allItems[currentIem]++ : allItems[currentIem] = 1;
         return allItems;
     }, {});
-  let totalPrice = 0;
-  const output = Object.keys(countedItems).map(input => {
-      const currentItem = loadAllItems().find(item => item.barcode === input);
-      const {name, unit, price} = currentItem;
-      const quantity = countedItems[input];
-      const currentPrice = price*quantity;
-      totalPrice = totalPrice + currentPrice;
-      return `名称：${name}，数量：${quantity}${unit}，单价：${price.toFixed(2)}(元)，小计：${currentPrice.toFixed(2)}(元)`
-  }).join("\n");
+    let totalPrice = 0;
+    const output = Object.keys(countedItems).map(input => {
+        const boughtIem = loadAllItems().find(item => item.barcode === input);
+        boughtIem.quantity = countedItems[input];
+        boughtIem.subtotalPrice = boughtIem.quantity * boughtIem.price;
+        totalPrice = totalPrice + boughtIem.subtotalPrice
+        return boughtIem;
+    }).map(item =>
+        `名称：${item.name}，数量：${item.quantity}${item.unit}，单价：${item.price.toFixed(2)}(元)，小计：${item.subtotalPrice.toFixed(2)}(元)`).join('\n');
     console.log(`***<没钱赚商店>收据***
 ${output}
 ----------------------
